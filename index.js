1c20670df7fe94d7451b3a5c6eb3c9d75c6c92f0
Vinicius Rangel <45022295+vr3lxrd@users.noreply.github.com>
2021-03-02 19:18:16 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({
+  auth: 'd10b1e0c458b25bf2fdbe8c1bfc6d3c833591ab7'
+})
+
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/static'))
+const port = process.env.PORT || 3000
+
+app.get('/', async (req, res) => {
+  let apiData = await octokit.request('GET /orgs/{org}/repos', {
+    org: 'ieeecsufabc'
+  })
+  let repos = []
+  apiData.data.forEach(element  => {
+    let page = element.homepage
+    repos.push({
+      name: element.name,
+      url: element.html_url,
+      principal_lang: element.language,
+      page: page,
+      description: element.description
+    })
+  })
+  res.render('index', {repos: repos})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
